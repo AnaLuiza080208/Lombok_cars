967806f7a9df4f6e21836cd015875c99f881a973
Ana <ana.luiza.kauffman@gmail.com>
2025-10-20 08:17:59 -0300

    Update

--- src/main/java/br/edu/ifpr/cars/domain/Driver.java
+++ src/main/java/br/edu/ifpr/cars/domain/Driver.java
@@ -17,4 +17,34 @@ public class Driver {
     String name;
     LocalDate birthDate;
 
+    public Driver(LocalDate birthDate, Long id, String name) {
+        this.birthDate = birthDate;
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
 }
